9c6e8a70cdd5913fed850cca36bbd9c6f00431ff
JordanC-137 <jordanasiama3@gmail.com>
2023-08-20 22:48:41 +0100

    Added Grid class, some testing of Tunnel theory

---
+++ grid.py
@@ -0,0 +1,4 @@
+from . import Chamber, Tunnel
+
+class Grid():
+    pass
--- tunnel.py
+++ tunnel.py
@@ -1,20 +1,34 @@
+import re
+
+
 class Tunnel:
-    def __init__(self, a, b, value):
+    def __init__(self, a, b):
         self.a = a
         self.b = b
-        self.value = value
         
-    def connects(self):
+    def connection(self):
         if self.a < self.b:
             return (self.a, self.b)
         else:
             return (self.b,self.a)
         
 
+    def __eq__(self, other):
+        if isinstance(other, Tunnel):
+            if self.connection() == other.connection():
+                return True
+        else:
+            return False
+
     def __str__(self):
-        return str(self.connects())
+        return str(self.connection())
 
 
-t = Tunnel((1, 1), (1,0))
+t1 = Tunnel((1, 1), (1,0))
+t2 = Tunnel((1, 0), (1,1))
+t3 = Tunnel((1, 0), (1,2))
+t4 = Tunnel((1, 0), (1,3))
 
-print(t)
+tunnel_list = [t3, t2, t4]
+if t1 in tunnel_list:
+    print("Tunnel")
