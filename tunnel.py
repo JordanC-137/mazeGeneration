22606ce7877218729ffb42e5360e38cb3848ec14
JordanC-137 <jordanasiama3@gmail.com>
2023-08-21 22:48:21 +0100

    Grid, chamber and tunnel classes struggling to co-operate

--- tunnel.py
+++ tunnel.py
@@ -2,9 +2,10 @@ import re
 
 
 class Tunnel:
-    def __init__(self, a, b):
+    def __init__(self, a, b, value):
         self.a = a
         self.b = b
+        self.value = value
         
     def connection(self):
         if self.a < self.b:
@@ -23,7 +24,10 @@ class Tunnel:
     def __str__(self):
         return str(self.connection())
 
+    def __hash__(self):
+        return self.connection()
 
+"""
 t1 = Tunnel((1, 1), (1,0))
 t2 = Tunnel((1, 0), (1,1))
 t3 = Tunnel((1, 0), (1,2))
@@ -32,3 +36,4 @@ t4 = Tunnel((1, 0), (1,3))
 tunnel_list = [t3, t2, t4]
 if t1 in tunnel_list:
     print("Tunnel")
+"""
