b0df8dec7de22809e641d2d1e8832cbb801ba0c5
JordanC-137 <jordanasiama3@gmail.com>
2023-09-06 20:22:04 +0100

    Completed add_connection, added Exception in case of foriegn node called. As well  as a display_grid function

--- Matrix.py
+++ Matrix.py
@@ -4,14 +4,19 @@ import random
 print("Hello World")
 
 
-class Matrix():
+class Matrix:
     def __init__(self):
         self.nodes = list(itertools.product([0,1,2], repeat = 2))
         self.grid = [[None for i in range(len(self.nodes))] for i in range(len(self.nodes))]
         print(self.nodes)
-        print(self.grid)
+        self.display_grid()
 
-    def add_connection(self, node1, node2,weight = None):
+    def display_grid(self):
+        [print(i) for i in self.grid]
+    
+    def add_connection(self, node1, node2, weight = None):
+        if node1 not in self.nodes or node2 not in self.nodes:
+            raise IndexError("One of these nodes is not in the grid")
         pos1 = self.nodes.index(node1)
         pos2 = self.nodes.index(node2)
         if weight:
@@ -21,7 +26,7 @@ class Matrix():
             random_value = random.randint(0,20)
             self.grid[pos1][pos2] = random_value
             self.grid[pos2][pos1] = random_value
-        print(self.grid)
+        self.display_grid()
 
 m = Matrix()
 print()
