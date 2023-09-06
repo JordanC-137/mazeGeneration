e2f6e44a05c59d55e762a58ebaf3ccece19ac889
JordanC-137 <jordanasiama3@gmail.com>
2023-09-06 18:08:55 +0100

    Re-thought adjacency matrix and began implementing add_connection

--- Matrix.py
+++ Matrix.py
@@ -1,16 +1,23 @@
 import itertools
+import random
 
 print("Hello World")
 
 
 class Matrix():
     def __init__(self):
-        self.grid = [[None for i in range(3)] for i in range(3)]
         self.nodes = list(itertools.product([0,1,2], repeat = 2))
+        self.grid = [[None for i in range(len(self.nodes))] for i in range(len(self.nodes))]
+        print(self.nodes)
+        print(self.grid)
 
-    def add_connection(self,weight = None):
-        pass
+    def add_connection(self, node1, node2,weight = None):
+        pos1 = self.nodes.index(node1)
+        pos2 = self.nodes.index(node2)
+        if weight:
+            self.grid[pos1][pos2] = weight
+            self.grid[pos1][pos2] = weight
+        print(self.grid)
 
 m = Matrix()
-print(m.nodes)
-#print(m.grid)
+m.add_connection((0,0), (0,1), 5)
