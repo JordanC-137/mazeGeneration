76df818c515b2ca4979db9f775695f3b7434820b
JordanC-137 <jordanasiama3@gmail.com>
2023-09-13 17:58:41 +0100

    Written test for adding connection, needs work

--- test_Matrix.py
+++ test_Matrix.py
@@ -1,13 +1,16 @@
-"""from maze import Matrix
 
-from Matrix import Matrix """
 import unittest
 from maze.Matrix import Matrix
 
 class TestMod(unittest.TestCase):
-    def test_module(self):
+    def test_initialisation(self):
         m = Matrix()
         self.assertEqual(m.nodes, [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)])
+    
+    def test_connections(self):
+        m = Matrix()
+        m.add_connection((0,2), (0,1), 1)
+        self.assertEqual(m.traverse_connections(), [[1, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]])
 
 if __name__ == '__main__':
     unittest.main()
