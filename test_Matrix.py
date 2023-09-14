dcb7f339eee8f4300f367d2218a12d3183a12375
JordanC-137 <jordanasiama3@gmail.com>
2023-09-14 20:59:15 +0100

    Added more basic test for adding connection

--- test_Matrix.py
+++ test_Matrix.py
@@ -3,10 +3,22 @@ import unittest
 from maze.Matrix import Matrix, Connection
 
 class TestMod(unittest.TestCase):
-    def test_initialisation(self):
+    def test_initial_nodes(self):
         m = Matrix()
         self.assertEqual(m.nodes, [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)])
     
+    def test_initial_grid(self):
+        m = Matrix()
+        ls = [[None for i in range(9)] for j in range(9)]
+        self.assertEqual(m.grid, ls)
+
+    def test_add_connection(self):
+        m = Matrix([1,2,3,4])
+        m.add_connection(2,3,4)
+        m.add_connection(4, 1, 10)
+        ls = [[None, None, None, 10],[None, None, 4, None],[None, 4, None, None],[10, None, None, None]]
+        self.assertEqual(m.grid, ls)
+
     def test_connections(self):
         m = Matrix([1,2,3,4])
         m.add_connection(1, 3, 5)
