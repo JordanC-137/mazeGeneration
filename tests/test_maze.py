7727802143e16a059abe5922103186a53dafa9fe
JordanC-137 <jordanasiama3@gmail.com>
2023-09-25 14:33:54 +0100

    Began writing mst test

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -28,6 +28,16 @@ class TestMaze(unittest.TestCase):
         ls = cross_set_connections(s1, s2, vals)
         ls = [(i.x, i.y) for i in ls]
         self.assertEqual(ls, [(1, 2), (3, 2)])
+    
+    def test_mst(self):
+        m = Matrix([0, 1, 2, 3, 4])
+        m.add_connection(0,1,8)
+        m.add_connection(2,0,5)
+        m.add_connection(2,1,9)
+        m.add_connection(3,1,11)
+        m.add_connection(2,4,10)
+        m.add_connection(2,3,15)
+        m.add_connection(3,4,7)
         
 
 if __name__ == '__main__':
