f4fcd4e2da18b20a4c6a3d923090c2c699fb9259
JordanC-137 <jordanasiama3@gmail.com>
2023-09-28 16:28:25 +0100

    Added test for mst1's E set

--- tests/test_mst1.py
+++ tests/test_mst1.py
@@ -23,6 +23,19 @@ class TestMST1(unittest.TestCase):
         expect.append(Connection(2, 4, 10))
         expect.append(Connection(3, 4, 7))
         self.assertEqual(m.traverse_connections(), expect)
+    
+    def test_mst1_E(self):
+        m = Matrix([0, 1, 2, 3, 4])
+        m.add_connection(0,1,8)
+        m.add_connection(2,0,5)
+        m.add_connection(2,1,9)
+        m.add_connection(3,1,11)
+        m.add_connection(2,4,10)
+        m.add_connection(2,3,15)
+        m.add_connection(3,4,7)
+        ls = m.traverse_connections()
+        ls = cross_set_connections([0], [1,2,3,4], ls)
+        self.assertEqual(ls, [Connection(0, 1, 8), Connection(0, 2, 5)])
 
 if __name__ == '__main__':
     unittest.main()
