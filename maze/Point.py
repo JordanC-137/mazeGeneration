b2f61f5c192dd08ca89d60e0f59a1d5aa364eae3
Jordan <jordanasiama3@gmail.com>
2026-06-28 22:52:15 +0100

    Point constructor for tuple

--- maze/Point.py
+++ maze/Point.py
@@ -3,6 +3,10 @@ class Point():
         self.x = x
         self.y = y
     
+    def __init__(self, coordinate):
+        self.x = coordinate[0]
+        self.y = coordinate[1]
+
     def __eq__(self, other_point):
         return self.x == other_point.x and self.y == other_point.y
 
