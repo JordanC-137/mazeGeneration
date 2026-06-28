aff763ccac468829be83481fe43c3d98bf46d992
Jordan <66384172+JordanC-137@users.noreply.github.com>
2026-06-28 22:01:34 +0100

    New Connection and Point classes (#4)
    * New Point class
    
    * New Connection class and Point hash function
    
    * Re-named initial Connection Tuple to allow less confusing re-factor
    
    * Add weight component to new Connection class

---
+++ maze/Connection.py
@@ -0,0 +1,13 @@
+from Point import Point
+
+class Connection:
+    def __init__(self, point1: Point, point2: Point, weight: int):
+        self.weight = weight
+        self.points = {point1, point2}
+
+    def __eq__(self, other_connection):
+        if self.points == other_connection.points:
+            return True
+        else:
+            return False
+    
---
+++ maze/Point.py
@@ -0,0 +1,10 @@
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __eq__(self, other_point):
+        return self.x == other_point.x and self.y == other_point.y
+
+    def __hash__(self):
+        return(hash(f"{self.x}.{self.y}"))
